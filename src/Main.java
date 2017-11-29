fc863f29a9487aa98ead0b0601dcfaac99883c4d
Victor <victor@sene.ovh>
2017-11-29 18:09:05 +0100

    create Thread to detect obstacle

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,20 @@ public class Main {
     public static void main(String[] args){
         SwagBot bot = new SwagBot(MotorPort.B, MotorPort.C, SensorPort.S2, SensorPort.S3);
         bot.calibration();
+
         LineFollower line_follower = new LineFollower(bot);
         Thread line_follower_thread = new Thread(line_follower);
         line_follower_thread.start();
+        BarriersDetect barriers_detector = new BarriersDetect(bot);
+        Thread barriers_detector_thread = new Thread(barriers_detector);
+        barriers_detector_thread.start();
+
         Button.waitForAnyPress();
         line_follower.terminate();
+        barriers_detector.terminate();
     }
 
     public static class LineFollower implements Runnable {
-
         SwagBot robot;
         LejosPID PID;
         float BASE_SPEED = 500;
@@ -35,8 +40,38 @@ public class Main {
         public void run() {
             while(running){
                 float direction = PID.getOutput(robot.mean_rgb() * 1.f/robot.getWhite());
-                System.out.println(direction);
+                //System.out.println(direction);
                 robot.speed((int)(BASE_SPEED - (direction * BASE_SPEED)), (int)(BASE_SPEED + (direction * BASE_SPEED)));
+            }
+        }
+
+    }
+
+    public static class BarriersDetect implements Runnable {
+
+        SwagBot robot;
+
+        private volatile boolean running = true;
+
+        public BarriersDetect(SwagBot robot) {
+            this.robot = robot;
+        }
+
+        public void terminate() {
+            running = false;
+        }
+
+        @Override
+        public void run() {
+            int i = 0;
+            while(running){
+                float dist = robot.distance(3);
+                System.out.println("Dist. "+ i + " : " + dist);
+                i++;
+                if (dist <= 30){
+                    robot.stop();
+                    System.out.println("Robot has STOP !");
+                }
             }
         }
 
