7ea8112cce9de7321a04f5ee59fbb43a6c61bc06
Victor <victor@sene.ovh>
2017-12-14 01:36:26 +0100

    test MQTT thread - need to be thoughtful

--- src/Main.java
+++ src/Main.java
@@ -13,21 +13,6 @@ import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 public class Main {
     public static void main(String[] args){
-        EV3IRSensor ir = new EV3IRSensor(SensorPort.S2);
-        SampleProvider sp = ir.getDistanceMode();
-        MQTTFilter mqtt = null;
-        try {
-            mqtt = new MQTTFilter(sp, "/ev3/test", "tcp://192.168.43.24:1883", "RobotSwag", 0);
-        } catch (MqttException e) {
-            e.printStackTrace();
-        }
-        float[] sample = new float[sp.sampleSize()];
-        while (Button.ESCAPE.isUp()) {
-            mqtt.fetchSample(sample, 0);
-            Delay.msDelay(100);
-        }
-        ir.close();
-
         Boss boss = Boss.getInstance();
         Thread boss_thread = new Thread(boss);
         boss_thread.start();
