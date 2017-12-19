c2a31cbe38a3776e7707f213ac2490a701aa69f8
Victor <victor@sene.ovh>
2017-12-19 19:49:45 +0100

    clean up main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,4 @@
 import lejos.hardware.Button;
-import lejos.hardware.port.MotorPort;
-import lejos.hardware.port.SensorPort;
-import lejos.hardware.sensor.EV3IRSensor;
-import lejos.robotics.SampleProvider;
-import lejos.robotics.filter.AbstractFilter;
-import lejos.utility.Delay;
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.eclipse.paho.client.mqttv3.MqttException;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
-import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 public class Main {
     public static void main(String[] args){
