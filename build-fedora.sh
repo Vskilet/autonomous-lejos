87d03436cd1ab09d552e51f4efea41b987f81ddc
Victor <victor@sene.ovh>
2017-12-20 16:47:42 +0100

    sending to LeJOS script for fedora

---
+++ build-fedora.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ 
+LEJOS_LIB=$1
+LEJOS_IP=$2
+
+jar -uvmf META-INF/MANIFEST.MF autonomous-lejos.jar
+ev3scpupload -n $LEJOS_IP lib/org.eclipse.paho.client.mqttv3-1.2.0.jar /home/lejos/lib/
+ev3scpupload -n $LEJOS_IP autonomous-lejos.jar /home/lejos/programs
+
