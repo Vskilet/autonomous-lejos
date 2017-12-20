a17926b683160d9f8914cfd41f7de46f1602950d
Victor <victor@sene.ovh>
2017-12-20 17:32:40 +0100

    modified build for fedora

--- build-fedora.sh
+++ build-fedora.sh
@@ -5,5 +5,6 @@ LEJOS_IP=$2
 
 jar -uvmf META-INF/MANIFEST.MF autonomous-lejos.jar
 ev3scpupload -n $LEJOS_IP lib/org.eclipse.paho.client.mqttv3-1.2.0.jar /home/lejos/lib/
+ev3scpupload -n $LEJOS_IP lib/gson-2.8.2.jar /home/lejos/lib/
 ev3scpupload -n $LEJOS_IP autonomous-lejos.jar /home/lejos/programs
 
