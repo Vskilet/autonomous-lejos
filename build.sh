97bd47eea10112e261cb47b6a62aaab68f744114
nyanloutre <paul@nyanlout.re>
2017-12-20 15:36:50 +0100

    Ajout librairie Gson

--- build.sh
+++ build.sh
@@ -3,9 +3,10 @@
 LEJOS_LIB=$1
 LEJOS_IP=$2
 
-/usr/lib/jvm/java-7-openjdk/bin/javac -cp $LEJOS_LIB:lib/org.eclipse.paho.client.mqttv3-1.2.0.jar -d . src/*.java src/threads/*.java
+/usr/lib/jvm/java-7-openjdk/bin/javac -cp $LEJOS_LIB:lib/org.eclipse.paho.client.mqttv3-1.2.0.jar:lib/gson-2.8.2.jar -d . src/*.java src/threads/*.java
 jar -cvmf META-INF/MANIFEST.MF autonomous-lejos.jar *.class
 rm *.class
 
 ev3scpupload -n $LEJOS_IP lib/org.eclipse.paho.client.mqttv3-1.2.0.jar /home/lejos/lib/
+ev3scpupload -n $LEJOS_IP lib/gson-2.8.2.jar /home/lejos/lib/
 ev3scpupload -n $LEJOS_IP autonomous-lejos.jar /home/lejos/programs
